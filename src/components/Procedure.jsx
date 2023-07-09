58cb830ab6fbe7b19ae5b83855cc28b974c0ceb2
Ashutosh Jha <98846260+ashdude1401@users.noreply.github.com>
2023-07-10 02:28:27 +0530

    procedure section edited

--- src/components/Procedure.jsx
+++ src/components/Procedure.jsx
@@ -1,65 +1,297 @@
-import '../styles/Procedure.css'
+// import "../styles/Procedure.css";
 
-import proc1 from '../images/proc11.png'
-import proc2 from '../images/proc1.png'
-import proc3 from '../images/img1.png'
+// import proc1 from "../images/proc11.png";
+// import proc2 from "../images/proc1.png";
+// import proc3 from "../images/img1.png";
+
+// const Procedure = () => {
+//   return (
+//     <div id="procedure" className="bg-violet-500 text-white">
+//       <div className="procedure-container">
+//         <h1 className="flex justify-center">Our Process</h1>
+
+//         {/* 1st procedure */}
+//         <section className="big-feature-section">
+//           <div className="container flex big-feature-container">
+//             <div className="feature-img feature-img-right">
+//               <img src={proc1} alt="img" />
+//             </div>
+//             <div className="feature-desc flex">
+//               <div className="text-container">
+//                 <h3>List your meds</h3>
+//                 <p>
+//                   Individuals and organizations donate their surplus medicine
+//                   instead of destroying it.
+//                 </p>
+//               </div>
+//             </div>
+//           </div>
+//         </section>
+
+//         {/* 2nd procedure */}
+//         <section className="big-feature-section">
+//           <div
+//             className="container flex big-feature-container"
+//             id="second-big-feature-container"
+//           >
+//             <div className="feature-img feature-img-left">
+//               <img src={proc3} alt="img" />
+//             </div>
+//             <div className="feature-desc flex">
+//               <div className="text-container">
+//                 <h3>Collection of listed meds</h3>
+//                 <p>
+//                   Medi-Connect matches donated medicine to community partners
+//                   who need it.
+//                 </p>
+//               </div>
+//             </div>
+//           </div>
+//         </section>
+
+//         {/*  3rd procedure*/}
+//         <section className="big-feature-section">
+//           <div className="container flex big-feature-container">
+//             <div className="feature-img feature-img-right">
+//               <img src={proc2} alt="img" />
+//             </div>
+//             <div className="feature-desc flex">
+//               <div className="text-container">
+//                 <h3>Finally, handing over</h3>
+//                 <p>
+//                   Community partners verify the quality of donated medicines
+//                   and prescriptions, then deliver them to patients who
+//                   otherwise wouldn’t have access.
+//                 </p>
+//               </div>
+//             </div>
+//           </div>
+//         </section>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Procedure;
+// import "../styles/Procedure.css";
+
+// import proc1 from "../images/proc11.png";
+// import proc2 from "../images/proc1.png";
+// import proc3 from "../images/img1.png";
+
+// const Procedure = () => {
+//   return (
+//     <div id="procedure" className="bg-violet-500 text-white">
+//       <div className="procedure-container">
+//         <h1 className="flex justify-center">Our Process</h1>
+
+//         {/* 1st procedure */}
+//         <section className="big-feature-section">
+//           <div className="container flex big-feature-container">
+//             <div className="feature-img feature-img-right">
+//               <img src={proc1} alt="img" />
+//             </div>
+//             <div className="feature-desc flex items-center justify-center">
+//               <div className="text-container px-4">
+//                 <h3>List your meds</h3>
+//                 <p>
+//                   Individuals and organizations donate their surplus medicine
+//                   instead of destroying it.
+//                 </p>
+//               </div>
+//             </div>
+//           </div>
+//         </section>
+
+//         {/* 2nd procedure */}
+//         <section className="big-feature-section">
+//           <div
+//             className="container flex big-feature-container"
+//             id="second-big-feature-container"
+//           >
+//             <div className="feature-desc flex items-center justify-center">
+//               <div className="text-container px-4">
+//                 <h3>Collection of listed meds</h3>
+//                 <p>
+//                   Medi-Connect matches donated medicine to community partners
+//                   who need it.
+//                 </p>
+//               </div>
+//             </div>
+//             <div className="feature-img feature-img-left">
+//               <img src={proc3} alt="img" />
+//             </div>
+//           </div>
+//         </section>
+
+//         {/*  3rd procedure*/}
+//         <section className="big-feature-section">
+//           <div className="container flex big-feature-container">
+//             <div className="feature-img feature-img-right">
+//               <img src={proc2} alt="img" />
+//             </div>
+//             <div className="feature-desc flex items-center justify-center">
+//               <div className="text-container px-4">
+//                 <h3>Finally, handing over</h3>
+//                 <p>
+//                   Community partners verify the quality of donated medicines
+//                   and prescriptions, then deliver them to patients who
+//                   otherwise wouldn’t have access.
+//                 </p>
+//               </div>
+//             </div>
+//           </div>
+//         </section>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Procedure;
+// import "../styles/Procedure.css";
+
+// import proc1 from "../images/proc11.png";
+// import proc2 from "../images/proc1.png";
+// import proc3 from "../images/img1.png";
+
+// const Procedure = () => {
+//   return (
+//     <div id="procedure" className="bg-violet-500 text-white">
+//       <div className="procedure-container">
+//         <h1 className="flex justify-center">Our Process</h1>
+
+//         {/* 1st procedure */}
+//         <section className="big-feature-section">
+//           <div className="container flex big-feature-container">
+//             <div className="feature-img feature-img-right justify-center">
+//               <img src={proc1} alt="img" />
+//             </div>
+//             <div className="feature-desc flex items-center justify-center">
+//               <div className="text-container px-4">
+//                 <h3 className="font-montserrat mb-2">List your meds</h3>
+//                 <p className="font-inter">
+//                   Individuals and organizations donate their surplus medicine
+//                   instead of destroying it.
+//                 </p>
+//               </div>
+//             </div>
+//           </div>
+//         </section>
+
+//         {/* 2nd procedure */}
+//         <section className="big-feature-section">
+//           <div
+//             className="container flex big-feature-container"
+//             id="second-big-feature-container"
+//           >
+//             <div className="feature-desc flex items-center justify-center">
+//               <div className="text-container px-4">
+//                 <h3 className="font-montserrat mb-2">Collection of listed meds</h3>
+//                 <p className="font-inter">
+//                   Medi-Connect matches donated medicine to community partners
+//                   who need it.
+//                 </p>
+//               </div>
+//             </div>
+//             <div className="feature-img feature-img-left justify-center">
+//               <img src={proc3} alt="img" />
+//             </div>
+//           </div>
+//         </section>
+
+//         {/*  3rd procedure*/}
+//         <section className="big-feature-section">
+//           <div className="container flex big-feature-container">
+//             <div className="feature-img feature-img-right justify-center">
+//               <img src={proc2} alt="img" />
+//             </div>
+//             <div className="feature-desc flex items-center justify-center">
+//               <div className="text-container px-4">
+//                 <h3 className="font-montserrat mb-2">Finally, handing over</h3>
+//                 <p className="font-inter">
+//                   Community partners verify the quality of donated medicines
+//                   and prescriptions, then deliver them to patients who
+//                   otherwise wouldn’t have access.
+//                 </p>
+//               </div>
+//             </div>
+//           </div>
+//         </section>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Procedure;
+import "../styles/Procedure.css";
+
+import proc1 from "../images/proc11.png";
+import proc2 from "../images/proc1.png";
+import proc3 from "../images/img1.png";
 
 const Procedure = () => {
   return (
     <div id="procedure" className="bg-violet-500 text-white">
       <div className="procedure-container">
-        <h1 className="flex justify-center">Our Process</h1>
+        <h1 className="flex justify-center text-6xl">Our Process</h1>
+
         {/* 1st procedure */}
         <section className="big-feature-section">
           <div className="container flex big-feature-container">
-            <div className="feature-img">
+            <div className="feature-img feature-img-right justify-center mx-auto w-3/5">
               <img src={proc1} alt="img" />
             </div>
-            <div className="feature-desc flex">
-              <h3>List your meds</h3>
-              <p>
-                Individuals and organizations donate their surplus medicine
-                instead of destroying it.
-              </p>
+            <div className="feature-desc flex items-center justify-center w-2/5">
+              <div className="text-container px-4">
+                <h3 className="font-extrabold text-5xl font-heading mb-2">List your meds</h3>
+                <p className="font-normal text-xl p-5">
+                  Don't let your unused or unexpired medicine go to waste. List them on MediConnect and help those in need. India faces many public health challenges, especially for the poor. Your donation can make a tangible impact.
+                </p>
+              </div>
             </div>
           </div>
         </section>
+
         {/* 2nd procedure */}
         <section className="big-feature-section">
           <div
             className="container flex big-feature-container"
             id="second-big-feature-container"
           >
-            <div className="feature-img">
-              <img src={proc3} alt="img" />
+            <div className="feature-desc flex items-center justify-center w-2/5">
+              <div className="text-container px-4">
+                <h3 className="font-extrabold text-5xl font-heading mb-2">Collection of listed meds</h3>
+                <p className="font-normal text-xl p-5">
+                  Our team collects the listed medicines and verifies their quality before matching them with community partners. India's health system is underfunded, inefficient, and underregulated. Your donation can help bridge the gap.
+                </p>
+              </div>
             </div>
-            <div className="feature-desc flex">
-              <h3>Collection of listed meds</h3>
-              <p>
-                Medi-Connect matches donated medicine to community partners who
-                need it
-              </p>
+            <div className="feature-img feature-img-left justify-center mx-auto w-3/5">
+              <img src={proc3} alt="img" />
             </div>
           </div>
         </section>
+
         {/*  3rd procedure*/}
         <section className="big-feature-section">
           <div className="container flex big-feature-container">
-            <div className="feature-img">
+            <div className="feature-img feature-img-right justify-center mx-auto w-3/5">
               <img src={proc2} alt="img" />
             </div>
-            <div className="feature-desc flex">
-              <h3>Finally, handing over</h3>
-              <p>
-                Community partners verifies the quality of donated medicines and
-                prescription, then delivers to patients who otherwise wouldn’t
-                have access.
-              </p>
+            <div className="feature-desc flex items-center justify-center w-2/5">
+              <div className="text-container px-4">
+                <h3 className="font-extrabold text-5xl font-heading mb-2">Finally, handing over</h3>
+                <p className="font-normal text-xl p-5">
+                  The donated medicines are delivered to patients in need, providing them with a lifeline they otherwise wouldn't have had. India suffers from poor sanitation and water supply, which contribute to many deaths and diseases. Your donation can help save lives.
+                </p>
+              </div>
             </div>
           </div>
         </section>
       </div>
     </div>
-  )
-}
-export default Procedure
+  );
+};
+
+export default Procedure;
+
