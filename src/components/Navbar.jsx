189c872fa48682cc9fe3615fb8a66b0f3f9ecfa7
AnushkaKohli <102284552+AnushkaKohli@users.noreply.github.com>
2023-07-10 12:39:07 +0530

    changes applied

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,6 +11,10 @@ const Navbar = () => {
   const handleSignup = ()=>{
     navigate('/signup')
   }
+
+  const handleHome = () => {
+    navigate('/')
+  }
   
   const [showModal, setShowModal] = useState(false);
 
@@ -34,16 +38,16 @@ const closeModal = () => {
       <div className="hidden md:flex">
         <div
           className="flex flex-col gap-8 rounded-lg border border-gray-100 bg-black p-4 md:mt-0 md:flex-row md:border-0 md:bg-white md:text-sm md:font-medium" style={{paddingLeft: "200px", fontSize: "17px"}}>
-          <a href="#Home"
+          <a href="#Home" onClick={handleHome}
             className="block rounded py-2 pr-4 pl-3 font-heading text-gray-700 hover:bg-gray-100 dark:text-black dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-violet-500 md:dark:hover:bg-transparent">Home</a>
 
-          <a href="#aboutUs"
+          <a href="#aboutUs" onClick={handleHome}
             className="block rounded py-2 pr-4 pl-3 font-heading text-gray-700 hover:bg-gray-100 dark:text-black dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-violet-500 md:dark:hover:bg-transparent">About Us</a>
 
-          <a href="#procedure"
+          <a href="#procedure" onClick={handleHome}
             className="block rounded py-2 pr-4 pl-3 font-heading text-gray-700 hover:bg-gray-100 dark:text-black dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-violet-500 md:dark:hover:bg-transparent">Procedure</a>
 
-          <a href="#contactUs"
+          <a href="#contactUs" onClick={handleHome}
             className="block rounded py-2 pr-4 pl-3 font-heading text-gray-700 hover:bg-gray-100 dark:text-black dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-violet-500 md:dark:hover:bg-transparent">Contact Us</a>
         </div>
       </div>
@@ -53,10 +57,10 @@ const closeModal = () => {
         className="text-md rounded-full bg-transparent px-4 py-2 font-medium mx-5 text-violet-500 transition-all disabled:bg-gray-400  border border-violet-500 hover:bg-violet-50">
         Login
       </button>
-      <button 
+      <button onClick={handleSignup}
         className="text-md rounded-full bg-violet-500 px-4 py-2 font-medium text-white transition-all disabled:bg-gray-400
         hover:bg-violet-600">
-        Sign Up
+        SignUp
       </button>
     </div>
   </div>
