8c4c878ee64a67f9860a012dce34c6d4885c37ae
Anurag Agarwal <anuragagarwal530@gmail.com>
2023-07-10 09:50:33 +0530

    add the logo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import About from './components/About';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Login from './components/Login';
-import SignUpChoice from './components/SignUpChoice';
-import SignupUser from './components/SignupUser';
-import SignupOrg from './components/SignupOrg';
-import MedicineForm from './components/MedicineForm';
-import ResetPassword from './components/ResetPassword';
-import ForgotPassword from './components/ForgotPassword';
-import UserProfile from './components/UserProfile';
-import Procedure from './components/Procedure';
-import MedicineDashboard from './components/MedicineDashboard';
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import SignUpChoice from "./components/SignUpChoice";
+import SignupUser from "./components/SignupUser";
+import SignupOrg from "./components/SignupOrg";
+import MedicineForm from "./components/MedicineForm";
+import ResetPassword from "./components/ResetPassword";
+import ForgotPassword from "./components/ForgotPassword";
+import UserProfile from "./components/UserProfile";
+import Procedure from "./components/Procedure";
+import MedicineDashboard from "./components/MedicineDashboard";
 
 export default function App() {
   return (
@@ -22,20 +22,27 @@ export default function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" exact element={<><Hero/> <About/> <Procedure/> <Contact /></>} />
+          <Route
+            path="/"
+            exact
+            element={
+              <>
+                <Hero /> <About /> <Procedure /> <Contact />
+              </>
+            }
+          />
           <Route path="/signupOrg" exact element={<SignupOrg />} />
-          <Route path="/signup" exact element={<SignUpChoice />}/>
+          <Route path="/signup" exact element={<SignUpChoice />} />
           <Route path="/login" exact element={<Login />} />
           <Route path="/userProfile" exact element={<UserProfile />} />
-          <Route path="/medicineDashboard" exact element={<MedicineDashboard/>} />
-          
+          <Route
+            path="/medicineDashboard"
+            exact
+            element={<MedicineDashboard />}
+          />
         </Routes>
-<<<<<<< HEAD
         <Footer />
         {/* <ProfilePage/> */}
-=======
-        {/* <Footer /> */}
->>>>>>> 6433a5afdd5594d69de73f1606f678ed555a7c09
       </Router>
       {/* <Hero />
       <About />
@@ -48,5 +55,5 @@ export default function App() {
       {/* <ForgotPassword /> */}
       {/* <Footer /> */}
     </>
-  )
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,71 +1,50 @@
-import React, { useState } from 'react'
-import { FaLink } from 'react-icons/fa'
-import logo from '../Logos/logo.png'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { FaLink } from "react-icons/fa";
+import logo from "../Logos/logo.png";
+import { useNavigate } from "react-router-dom";
 // import SignupModal from '../components/SignupModal'
 const Navbar = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const handleLogin = () => {
-    navigate('/login')
-  }
+    navigate("/login");
+  };
   return (
     <nav className="bg-white px-6 py-2 shadow-lg">
-<<<<<<< HEAD
       <div className="flex items-center justify-between">
         <div className="flex items-center">
           <a href="/" className="flex items-center">
             <img src={logo} className="w-16" alt="Header Logo" />
             <p className="hidden self-center text-lg font-semibold sm:block pl-3">
-              <span style={{ display: 'inline' }}>Medi </span>
-              <FaLink style={{ display: 'inline' }} />
-              <span style={{ display: 'inline' }}> Connect</span>
+              <span style={{ display: "inline" }}>Medi </span>
+              <FaLink style={{ display: "inline" }} />
+              <span style={{ display: "inline" }}> Connect</span>
             </p>
           </a>
           <div className="hidden md:flex">
             <div
               className="flex flex-col gap-8 rounded-lg border border-gray-100 bg-black p-4 md:mt-0 md:flex-row md:border-0 md:bg-white md:text-sm md:font-medium"
-              style={{ paddingLeft: '200px', fontSize: '17px' }}
-            >
+              style={{ paddingLeft: "200px", fontSize: "17px" }}>
               <a
                 href="#"
-                className="block rounded py-2 pr-4 pl-3 font-heading text-gray-700 hover:bg-gray-100 dark:text-black dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-violet-500 md:dark:hover:bg-transparent"
-              >
+                className="block rounded py-2 pr-4 pl-3 font-heading text-gray-700 hover:bg-gray-100 dark:text-black dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-violet-500 md:dark:hover:bg-transparent">
                 Home
               </a>
-=======
-  <div className="flex items-center justify-between">
-    <div className="flex items-center">
-      <a href="/" className="flex items-center">
-        <img src={logo} className="w-16" alt="Header Logo" />
-        <p className="hidden self-center text-lg font-semibold sm:block pl-3">
-          Medi⇿Connect
-        </p>
-      </a>
-      <div className="hidden md:flex">
-        <div
-          className="flex flex-col gap-8 rounded-lg border border-gray-100 bg-black p-4 md:mt-0 md:flex-row md:border-0 md:bg-white md:text-sm md:font-medium" style={{paddingLeft: "200px", fontSize: "17px"}}>
-          <a href="#Home"
-            className="block rounded py-2 pr-4 pl-3 font-heading text-gray-700 hover:bg-gray-100 dark:text-black dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-violet-500 md:dark:hover:bg-transparent">Home</a>
->>>>>>> 6433a5afdd5594d69de73f1606f678ed555a7c09
 
               <a
                 href="#aboutUs"
-                className="block rounded py-2 pr-4 pl-3 font-heading text-gray-700 hover:bg-gray-100 dark:text-black dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-violet-500 md:dark:hover:bg-transparent"
-              >
+                className="block rounded py-2 pr-4 pl-3 font-heading text-gray-700 hover:bg-gray-100 dark:text-black dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-violet-500 md:dark:hover:bg-transparent">
                 About Us
               </a>
 
               <a
                 href="#procedure"
-                className="block rounded py-2 pr-4 pl-3 font-heading text-gray-700 hover:bg-gray-100 dark:text-black dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-violet-500 md:dark:hover:bg-transparent"
-              >
+                className="block rounded py-2 pr-4 pl-3 font-heading text-gray-700 hover:bg-gray-100 dark:text-black dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-violet-500 md:dark:hover:bg-transparent">
                 Procedure
               </a>
 
               <a
                 href="#contactUs"
-                className="block rounded py-2 pr-4 pl-3 font-heading text-gray-700 hover:bg-gray-100 dark:text-black dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-violet-500 md:dark:hover:bg-transparent"
-              >
+                className="block rounded py-2 pr-4 pl-3 font-heading text-gray-700 hover:bg-gray-100 dark:text-black dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-violet-500 md:dark:hover:bg-transparent">
                 Contact Us
               </a>
             </div>
@@ -74,21 +53,19 @@ const Navbar = () => {
         <div className="flex items-center">
           <button
             onClick={handleLogin}
-            className="text-md rounded-full bg-transparent px-4 py-2 font-medium mx-5 text-violet-500 transition-all disabled:bg-gray-400  border border-violet-500 hover:bg-violet-50"
-          >
+            className="text-md rounded-full bg-transparent px-4 py-2 font-medium mx-5 text-violet-500 transition-all disabled:bg-gray-400  border border-violet-500 hover:bg-violet-50">
             Login
           </button>
           <button
-            onClick={() => navigate('/signup')}
+            onClick={() => navigate("/signup")}
             className="text-md rounded-full bg-violet-500 px-4 py-2 font-medium text-white transition-all disabled:bg-gray-400
-        hover:bg-violet-600"
-          >
+        hover:bg-violet-600">
             Sign Up
           </button>
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
